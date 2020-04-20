bd932c5e45db38c5201f1d79163aef364f579da8
poms313 <pommine@free.fr>
2020-04-20 20:17:50 +0200

    ejection d'expo et utilisation des compasants du téléphone

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import React from 'react'
 import Navigation from './Navigation/Navigation'
 import { Provider } from 'react-redux'
 import Store from './Store/configureStore'
-import Test from './Components/test'
 
 export default class App extends React.Component {
   render() {
